1c6be6c3358264e179ddf7fe637059c843789fb7
gamwe59 <epicgamwe59@gmail.com>
2025-05-10 01:30:14 -0700

    changed speed of game to be a bit faster

--- Constants.java
+++ Constants.java
@@ -38,5 +38,5 @@ public class Constants {
     public static final String SELECT_MESSAGE_COL = "PLEASE ENTER COLUMN. SELECT FIRST SPACE THAT A PIECE APPEARS ON";
     public static final String INVALID_SPACE = "INVALID.";
 
-    public static final int MS = 250;
+    public static final int MS = 175;
 }
